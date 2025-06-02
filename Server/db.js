dac68d5737315c065da6049aac1c07361a03fe9d
LEOESP32 <compartiendochat@gmail.com>
2025-06-02 16:35:47 -0300

    modificacion de nombres base de datos sql

--- Server/db.js
+++ Server/db.js
@@ -14,9 +14,9 @@ db.serialize(() => {
   // Insertar productos iniciales si la tabla está vacía
   db.all("SELECT COUNT(*) as count FROM productos", (err, rows) => {
     if (rows[0].count === 0) {
-      db.run("INSERT INTO productos (id, nombre, precio, imagen) VALUES (11, 'Producto A', 20, 'imagenes/producto-A.jpg')");
-      db.run("INSERT INTO productos (id, nombre, precio, imagen) VALUES (13, 'Producto B', 35, 'imagenes/producto-B.jpg')");
-      db.run("INSERT INTO productos (id, nombre, precio, imagen) VALUES (15, 'Producto C', 50, 'imagenes/producto-C.jpg')");
+      db.run("INSERT INTO productos (id, nombre, precio, imagen) VALUES (11, 'Producto 11', 20, 'imagenes/producto-A.jpg')");
+      db.run("INSERT INTO productos (id, nombre, precio, imagen) VALUES (13, 'Producto 13', 35, 'imagenes/producto-B.jpg')");
+      db.run("INSERT INTO productos (id, nombre, precio, imagen) VALUES (15, 'Producto 15', 50, 'imagenes/producto-C.jpg')");
     }
   });
 });
